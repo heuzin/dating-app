13b6a4692b35bad8626650a06ac1a13bfaa6bbd7
Matheus Silva <msilvaqs@outlook.com>
2024-09-01 22:29:40 -0400

    Creating user onlines with signalR

---
+++ API/SignalR/PresenceHub.cs
@@ -0,0 +1,34 @@
+using System;
+using API.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.SignalR;
+
+[Authorize]
+public class PresenceHub(PresenceTracker presenceTracker) : Hub
+{
+    public override async Task OnConnectedAsync()
+    {
+        if (Context.User == null) throw new HubException("Cannot get curent user claim");
+
+        await presenceTracker.UserConnected(Context.User.GetUserName(), Context.ConnectionId);
+        await Clients.Others.SendAsync("UserIsOnline", Context.User?.GetUserName());
+
+        var currentUsers = await presenceTracker.GetOnlineUsers();
+        await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        if (Context.User == null) throw new HubException("Cannot get curent user claim");
+
+        await presenceTracker.UserDisconnected(Context.User.GetUserName(), Context.ConnectionId);
+        await Clients.Others.SendAsync("UserIsOffline", Context.User?.GetUserName());
+
+        var currentUsers = await presenceTracker.GetOnlineUsers();
+        await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
+
+        await base.OnDisconnectedAsync(exception);
+    }
+}
